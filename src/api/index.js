cc0f1ca5406d66a322e39e94665a7a8dcc85b4e9
César Jeanroy <cesar.jeanroy@gmail.com>
2018-10-18 20:41:54 -0400

    Added refills endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import user from './user'
 import auth from './auth'
+import refill from './refill'
 
 const router = new Router()
 
@@ -29,5 +30,6 @@ const router = new Router()
  */
 router.use('/users', user)
 router.use('/auth', auth)
+router.use('/refills', refill)
 
 export default router
