e4ce585592b77f0e434ce11fae53094a26d7523f
César Jeanroy <cesar.jeanroy@gmail.com>
2018-10-19 23:29:54 -0400

    added materialized

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
+      <div>
+        <header>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo center">Logo</a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+        </nav>
         </header>
+        <div className="container">
+          <Home/>
+        </div>
       </div>
     );
   }
