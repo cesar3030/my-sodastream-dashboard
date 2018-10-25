581daee0318428acb092d901c83c0d56dbdf4c6a
César Jeanroy <cesar.jeanroy@gmail.com>
2018-10-24 23:39:38 -0400

    Adding post request to local node server when a refill is done

--- raspberry-pi-button/refill_button.py
+++ raspberry-pi-button/refill_button.py
@@ -5,25 +5,31 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(25, GPIO.IN)
-
+url = "http://0.0.0.0:9000/refills"
 
 
 def new_refill(elapsed_time):
-    print elapsed_time
+    http_post(url, {"elapsedTime": elapsed_time})
     
-start_time=None
+def http_post(url, data):
+    post = urlencode(data)
+    req = urllib2.Request(url, post)
+    response = urllib2.urlopen(req)
+    return response.read()
 
-while True:
-    if GPIO.input(25):
-        GPIO.output(18, False);
-        if start_time:
-            elapsed_time = time.time() - start_time
-            new_refill(elapsed_time)
-            start_time = None
-            
-    else:
-        if not start_time:
-            start_time = time.time()
-        GPIO.output(18, True);
-    time.sleep(0.05)
+def listen_to_click_events():
+  start_time=None
+  while True:
+      if GPIO.input(25):
+          GPIO.output(18, False)
+          if start_time:
+              elapsed_time = time.time() - start_time
+              new_refill(elapsed_time)
+              start_time = None
+              
+      else:
+          if not start_time:
+              start_time = time.time()
+          GPIO.output(18, True)
+      time.sleep(0.05)
 
