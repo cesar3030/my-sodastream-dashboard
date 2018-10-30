849199181791239a6c06865603d04c23c553949e
César Jeanroy <cesar.jeanroy@gmail.com>
2018-10-29 22:48:26 -0400

    faking endpoint call and data parsing

--- react-client/src/components/RefillsChart.js
+++ react-client/src/components/RefillsChart.js
@@ -2,9 +2,50 @@ import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 
 class RefillsChart extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      labels:[],
+      data:[]
+    };
+  }
+  
+  jsonToLabelsAndDataArrays = (json) => {
+    const labels = [];
+    const data = [];
 
-    const data = {
+    for(let label in json) {
+      labels.push(label);
+      data.push(json[label]);
+    }
+
+    this.setState({
+      labels: labels,
+      data: data
+    })
+  }
+
+  mockRequest = () => {
+    const mockResponse = {
+      'Monday': 1,
+      'Tuesday': 2,
+      'Wednesday': 1,
+      'Thursday': 3,
+      'Friday': 4,
+      'Saturday': 7,
+      'Sunday': 3
+    };
+
+    setTimeout(() => this.jsonToLabelsAndDataArrays(mockResponse), 4000);
+  }
+
+  componentDidMount() {
+    this.mockRequest();
+  }
+
+  render = () => {
+
+    const config = {
       datasets: [
         {
           label: 'Number of bottles refilled',
@@ -25,17 +66,17 @@ class RefillsChart extends Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
+          data: this.state.data //[65, 59, 80, 81, 56, 55, 40]
         }
       ],
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+      labels: this.state.labels //['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     };
 
     return (
       <div className="col s12 m6">
         <div className="card-panel">
           <h5>Refills chart</h5>
-          <Line data={data}/>
+          <Line data={config}/>
         </div>
       </div>
     );
--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -1,10 +1,9 @@
-import { BOX_CLICK } from "../constants/action-types";
 
 const initialState = { };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case BOX_CLICK:
+    case "BOX_CLICK":
       return state;
     default:
       return state;
