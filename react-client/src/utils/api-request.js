87c233d36383a3f89728d7b51d472cd2302e513c
César Jeanroy <cesar.jeanroy@gmail.com>
2018-11-08 20:53:26 -0500

    utility class to make api requests

---
+++ react-client/src/utils/api-request.js
@@ -0,0 +1,25 @@
+export default class ApiRequest {
+  
+  static fetch(url, beginFunction, sucessfunction, errorFunction){
+    return dispatch => {
+      dispatch(beginFunction());
+      return fetch(`${process.env.REACT_APP_API_URL}${url}`)
+        .then(ApiRequest.handleErrors)
+        .then(res => res.json())
+        .then(value => {
+          dispatch(sucessfunction(value));
+          return value;
+        })
+        .catch(error => dispatch(errorFunction(error)));
+    };
+  }
+
+  static fetchObject
+
+  static handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+} 
