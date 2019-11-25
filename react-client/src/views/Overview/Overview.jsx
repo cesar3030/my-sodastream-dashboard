0965e38b0f047f25dd8b18e9003027dca39671e0
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2019-11-25 11:28:09 -0500

    displaying current month & current year refills count

--- react-client/src/views/Overview/Overview.jsx
+++ react-client/src/views/Overview/Overview.jsx
@@ -1,61 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchCurrentWeekRefillsPerDate, fetchCurrentWeekRefillsCount } from '../../actions/refills/currentWeekActions'
-import { fetchCurrentWeekUsageVolume } from '../../actions/usage/currentWeekActions'
-import { fetchCurrentReload } from '../../actions/reload/currentReloadActions';
-
 import {
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardTitle,
-  CardHeader
-} from "reactstrap";
+  fetchCurrentWeekRefillsPerDate,
+  fetchCurrentWeekRefillsCount
+} from "../../actions/refills/currentWeekActions";
+import { fetchCurrentMonthRefillsCount } from "../../actions/refills/currentMonthActions";
+import { fetchCurrentYearRefillsCount } from "../../actions/refills/currentYearActions";
+
+import { fetchCurrentWeekUsageVolume } from "../../actions/usage/currentWeekActions";
+import { fetchCurrentReload } from "../../actions/reload/currentReloadActions";
+
+import { Row, Col, Card, CardBody, CardTitle, CardHeader } from "reactstrap";
 // react plugin used to create charts
 import { Line, Doughnut } from "react-chartjs-2";
 // function that returns a color based on an interval of numbers
 
 import { PanelHeader, CardStat, CardCategory } from "components";
 
-import {
-  dashboardPanelChart,
-  doughnutChart
-} from "variables/charts.jsx";
+import { dashboardPanelChart, doughnutChart } from "variables/charts.jsx";
 
 const mapStateToProps = state => ({
   nbRefillsPerDate: state.refills.currentWeek.perDate.nbRefillsPerDate,
   dates: state.refills.currentWeek.perDate.dates,
   loading: state.refills.currentWeek.perDate.loading,
-  refillsCount: state.refills.currentWeek.count.value,
+  refillsCountCurrentWeek: state.refills.currentWeek.count.value,
+  refillsCountCurrentMonth: state.refills.currentMonth.count.value,
+  refillsCountCurrentYear: state.refills.currentYear.count.value,
   usageVolume: state.usage.currentWeek.volume.value,
-  reload: state.reloads.current,
+  reload: state.reloads.current
 });
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchData: () => {
-      dispatch(fetchCurrentWeekRefillsPerDate())
-      dispatch(fetchCurrentWeekRefillsCount())
-      dispatch(fetchCurrentWeekUsageVolume())
+      dispatch(fetchCurrentWeekRefillsPerDate());
+      dispatch(fetchCurrentWeekRefillsCount());
+      dispatch(fetchCurrentWeekUsageVolume());
+      dispatch(fetchCurrentMonthRefillsCount());
+      dispatch(fetchCurrentYearRefillsCount());
       dispatch(fetchCurrentReload());
     }
-  }
+  };
 };
 
-
 class Overview extends React.Component {
-  
   componentDidMount() {
-    const { fetchData } = this.props; 
+    const { fetchData } = this.props;
 
-    if(typeof fetchData === "function") {
+    if (typeof fetchData === "function") {
       fetchData();
     }
   }
-  
+
   render() {
-    const chartData = dashboardPanelChart.data(this.props.nbRefillsPerDate, this.props.dates, "Nb Refills");
+    const chartData = dashboardPanelChart.data(
+      this.props.nbRefillsPerDate,
+      this.props.dates,
+      "Nb Refills"
+    );
     const { overAllUsagePercentage } = this.props.reload;
     const remainingOverAllUsagePercentage = 100 - overAllUsagePercentage;
     return (
@@ -63,10 +65,7 @@ class Overview extends React.Component {
         <PanelHeader
           size="lg"
           content={
-            <Line
-              data={chartData}
-              options={dashboardPanelChart.options}
-            />
+            <Line data={chartData} options={dashboardPanelChart.options} />
           }
         />
         <div className="content">
@@ -75,28 +74,28 @@ class Overview extends React.Component {
               <Row>
                 <Col xs={12} md={6}>
                   <CardStat
-                  title='Refills this week'
-                  value={this.props.refillsCount}
-                  unit='Refills'
+                    title="Refills this week"
+                    value={this.props.refillsCountCurrentWeek}
+                    unit="Refills"
                   />
                 </Col>
                 <Col xs={12} md={6}>
                   <CardStat
-                  title='Usage this week'
-                  value={this.props.usageVolume}
-                  unit='Liters'
+                    title="Usage this week"
+                    value={this.props.usageVolume}
+                    unit="Liters"
                   />
                 </Col>
                 <Col xs={12} md={6}>
                   <CardStat
-                  title='Refill this week'
-                  value='3'
+                    title="Refills this Year"
+                    value={this.props.refillsCountCurrentYear}
                   />
                 </Col>
                 <Col xs={12} md={6}>
                   <CardStat
-                  title='Usage this week'
-                  value='45'
+                    title="Refills this month"
+                    value={this.props.refillsCountCurrentMonth}
                   />
                 </Col>
               </Row>
@@ -104,15 +103,16 @@ class Overview extends React.Component {
             <Col xs={12} md={4}>
               <Card className="card-chart">
                 <CardHeader>
-                  <CardCategory>Tendency</CardCategory>
-                  <CardTitle tag="h4">Reload usage</CardTitle>
+                  <CardCategory>Reload usage</CardCategory>
+                  {/* <CardTitle tag="h4">Reload usage</CardTitle> */}
                 </CardHeader>
                 <CardBody>
-                  <Doughnut data={doughnutChart(
-                      [overAllUsagePercentage,remainingOverAllUsagePercentage],
-                      ['Used CO2', 'Remaining CO2']
-                    )}>
-                  </Doughnut>
+                  <Doughnut
+                    data={doughnutChart(
+                      [overAllUsagePercentage, remainingOverAllUsagePercentage],
+                      ["Used CO2", "Remaining CO2"]
+                    )}
+                  />
                 </CardBody>
               </Card>
             </Col>
@@ -123,4 +123,7 @@ class Overview extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Overview);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Overview);
